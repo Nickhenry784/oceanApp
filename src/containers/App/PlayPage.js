e331e243e23b24c97f6cb6b9954263b7884dbf18
Chuc Nguyen <nhiennguyen1605@gmail.com>
2022-03-24 10:17:52 +0700

    edit playPage

--- src/containers/App/PlayPage.js
+++ src/containers/App/PlayPage.js
@@ -2,97 +2,59 @@ import React, { useEffect, useState, useRef } from 'react';
 import PropTypes, { element } from 'prop-types';
 import { View, Image, Animated } from 'react-native';
 import { connect } from 'react-redux';
-import Easing from 'react-native/Libraries/Animated/Easing';
+import { randomIntFromInterval } from 'utils/number';
 import { decrementTurn, setPlayState } from './actions';
 import { appStyle, firstStyle } from './style';
 
 function PlayPage({ dispatch, first }) {
-  const [time, setTime] = useState(1);
-  const pan = useRef(
-    new Animated.ValueXY({
-      x: Math.floor(Math.random() * 100) + 1,
-      y: Math.floor(Math.random() * 100) + 1,
-    }),
-  ).current;
+  const [time, setTime] = useState(2000);
+  const [position, setPosition] = useState({
+    top: 200,
+    left: 200,
+  });
+  const [isTurnLeft, setTurnLeft] = useState(randomIntFromInterval(0, 1));
+  const [isTurnTop, setTurnTop] = useState(randomIntFromInterval(0, 1));
 
   useEffect(() => {
-    const same = () => {
-      if (time > 0) {
-        setTime(time - 1);
-      }
-    };
-    const same1 = () => {
-      if (time === 0) {
-        dispatch(decrementTurn());
+    const timeEachTurn = 5;
+    const timeout = setTimeout(() => {
+      if (time <= 0) {
         dispatch(setPlayState(false));
+        dispatch(decrementTurn());
+        return false;
       }
-    };
-    const time1 = setTimeout(same, 1000);
-    const time2 = setTimeout(same1, 12000);
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(pan, {
-          toValue: {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-          },
-          duration: 1000,
-          delay: 3000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(pan, {
-          toValue: {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-          },
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(pan, {
-          toValue: {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-          },
-          duration: 1000,
-          delay: 2000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(pan, {
-          toValue: {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-          },
-          duration: 2000,
-          delay: 4000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(pan, {
-          toValue: {
-            x: Math.floor(Math.random() * 100) + 1,
-            y: Math.floor(Math.random() * 100) + 1,
-          },
-          duration: 3000,
-          delay: 6000,
-          useNativeDriver: true,
-        }),
-      ]),
-      {
-        iterations: 1,
-      },
-    ).start();
+      if (time % 100 === 0) setTurnLeft(randomIntFromInterval(0, 1));
+      if (time % 200 === 0) setTurnTop(randomIntFromInterval(0, 1));
+      setTime(time - timeEachTurn);
+      setPosition({
+        top:
+          randomIntFromInterval(0, 10) < 8
+            ? position?.top + randomIntFromInterval(0, 1) * (isTurnTop ? -1 : 1)
+            : position?.top,
+        left:
+          position?.left + randomIntFromInterval(0, 3) * (isTurnLeft ? -1 : 1),
+      });
+    }, timeEachTurn);
     return () => {
-      clearTimeout(time1);
-      clearTimeout(time2);
+      clearTimeout(timeout);
     };
-  }, [pan, time]);
+  }, [position]);
 
   return (
     <View style={appStyle.homeView}>
       <Animated.View
         style={[
-          { width: 30, height: 30, position: 'absolute', top: 0, left: 0 },
           {
-            transform: [{ translateX: pan.x }, { translateY: pan.y }],
+            width: 30,
+            height: 30,
+            position: 'absolute',
+            top: position?.top,
+            left: position?.left,
+            transform: [
+              {
+                rotateY: isTurnLeft ? '0deg' : '180deg',
+              },
+            ],
           },
         ]}>
         <Image source={first.image} style={appStyle.firstImage} />
---
+++ src/utils/number.js
@@ -0,0 +1,2 @@
+export const randomIntFromInterval = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
